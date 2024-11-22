1b9f322ea9d8511710d01d51b6904edcba3dfac0
paulaverion <163812351+paulaverion@users.noreply.github.com>
2024-11-22 13:25:21 +0000

    donee

--- Laboratory Activities/Laboratory Activity 3-{22}-{11}-{2024}/ArtistDemo.java
+++ Laboratory Activities/Laboratory Activity 3-{22}-{11}-{2024}/ArtistDemo.java
@@ -9,5 +9,20 @@ public class ArtistDemo {
         Singer singer = new Singer("Nayeon Im", "Korean", 29, "Music", Genre.POP);
         singer.displayInfo();
 
+        System.out.println();
+
+        Painter painter = new Painter("Yayoi Kusama", "Japanese", 95, "Painting", Medium.OIL);
+        painter.displayInfo();
+
+        System.out.println();
+
+        Writer writer = new Writer("Stephen King", "American", 77, "Writing", WritingStyle.FICTION);
+        writer.displayInfo();
+
+        System.out.println();
+        
+        Dancer dancer = new Dancer("Kim Yugyeom", "Korean", 27, "Dancing", DanceStyle.HIPHOP);
+        dancer.displayInfo();
+
     }
 }

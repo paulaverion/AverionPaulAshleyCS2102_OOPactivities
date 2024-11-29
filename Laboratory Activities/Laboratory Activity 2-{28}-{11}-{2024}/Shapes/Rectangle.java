6ac9daf90166bfb0d5e56b3aeb3cad2708c40a23
paulaverion <163812351+paulaverion@users.noreply.github.com>
2024-11-29 06:07:36 +0000

    lab 4 done(?)

---
+++ Laboratory Activities/Laboratory Activity 2-{28}-{11}-{2024}/Shapes/Rectangle.java
@@ -0,0 +1,23 @@
+class Rectangle extends Shape {
+    private double length, width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double getArea() {
+        return length * width;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (length + width);
+    }
+
+    @Override
+    public void printShapeType() {
+        System.out.println("Rectangle");
+    }
+}
---
+++ Laboratory Activities/Laboratory Activity 2-{28}-{11}-{2024}/Shapes/Shape.java
@@ -0,0 +1,8 @@
+abstract class Shape {
+    public abstract double getArea();
+    public abstract double getPerimeter();
+
+    public void printShapeType() {
+        System.out.println("Generic Shape");
+    }
+}
